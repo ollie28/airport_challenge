1eb4ad47771edc842624314bede4fb7e0a93fffd
Olive B <oliveb@Olives-MacBook-Pro.local>
2018-09-22 17:18:05 +0100

    airport and plane lib spec created

---
+++ spec/features/user_stories_spec.rb
@@ -0,0 +1,19 @@
+# Describe user stories do
+# As an air traffic controller
+# So I can get passengers to a destination
+# I want to instruct a plane to land at an airport
+# it 'so planes land at airports, instruct a plane to land' do
+  # airport = Airport.new
+  #plane = Plane.new
+  #expect { airport.land(plane) }.not_to_raise_error
+  #end
+
+  # As an air traffic controller
+  # So I can get passengers on the way to their destination
+  # I want to instruct a plane to take off from an airport
+  #it 'so planes take off from airports, instruct a plane to take off'
+  #airport = Airport.new
+  #plane = Plane.new
+#  expect { airport.take_off(plane) }.not_to_raise_error
+#  end
+#end
