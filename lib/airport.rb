c18f8e5d72a5d1618489c03eac3de9c6ad77ec7f
Olive B <oliveb@Olives-MacBook-Pro.local>
2018-09-22 19:01:52 +0100

    refactor of previous commit

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,22 @@
 class Airport
-  def land(plane)
+
+  def initialize(capacity)
+    @capacity = capacity
+    @planes = []
   end
+
+  def land(plane)
+    raise 'Cannot land plane: airport full' if full?
+    @planes << plane
+  end
+
   def take_off(plane)
+  end
+
+
+private
+
+  def full?
+    @planes.length >= @capacity
   end
 end
